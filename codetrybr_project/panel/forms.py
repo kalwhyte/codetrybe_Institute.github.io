cbf65c744a5ceb5f01aa0a49c2ec8eb26fe19848
kalwhyte <superkalel55@gmail.com>
2023-06-26 19:00:27 +0100

    toushes

--- codetrybr_project/panel/forms.py
+++ codetrybr_project/panel/forms.py
@@ -8,31 +8,29 @@ class AdminRegistrationForm(UserCreationForm):
     to create a User named Admin
     admin registration form 
     """
-    phone_number = forms.CharField(max_length=20)
+    phone_number = forms.CharField(max_length=11)
     email = forms.EmailField()
     address = forms.CharField(max_length=50)
 
 
 class TeacherRegistrationForm(UserCreationForm):
     """
-    adding extra field to the User created form
-    to create a User named Admin
-    Teacher registration form 
+    Teacher registration form fields
     """
-    phone_number = forms.CharField(max_length=20)
+    phone_number = forms.CharField(max_length=11)
     email = forms.EmailField()
     address = forms.CharField(max_length=50)
     
 
 class StudentRegistrationForm(UserCreationForm):
     """
-    adding extra field to the User created form
-    to create a User named Admin
-    Student registration form 
+    Student registration form fields
     """
-    phone_number = forms.CharField(max_length=20)
+    phone_number = forms.CharField(max_length=11)
     email = forms.EmailField()
     address = forms.CharField(max_length=50)
-
-
-
+    level = forms.CharField(max_length=50)
+    optional_subject = forms.CharField(max_length=50)
+    subject = forms.CharField(max_length=50)
+    dob = forms.DateField()
+    gender = forms.CharField(max_length=50)
--- codetrybr_project/panel/models.py
+++ codetrybr_project/panel/models.py
@@ -9,6 +9,7 @@ class Admin(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20,default="0802")
     address = models.CharField(max_length=50,default="Default address")
+
     def __str__(self):
         return self.user.username
 
@@ -18,7 +19,8 @@ class Teacher(models.Model):
     a model for Teachers table
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20,default="0802")
+    phone_number = models.CharField(max_length=20,default="+234-")
+    email = models.EmailField(max_length=50)
     address = models.CharField(max_length=50,default="Default address")
 
     def __str__(self):
@@ -30,8 +32,7 @@ class Subject(models.Model):
     subjects offered by students
     """
     name = models.CharField(max_length=30)
-    Teacher = models.ForeignKey(Teacher,on_delete=models.SET_NULL,null=True, blank=True,
-                                      related_name="subject_taught")
+    Teacher = models.ManyToManyField(Teacher,related_name="subject_taught")
     # class_offering =models.ForeignKey()
     def __str__(self):
         return self.name
@@ -42,8 +43,8 @@ class OptionalSubject(models.Model):
     optional subjects offered by student
     """
     name = models.CharField(max_length=30)
-    Teacher = models.ForeignKey(Teacher,on_delete=models.SET_NULL,null=True, blank=True,
-                                      related_name="optional_subject_taught")
+    Teacher = models.ManyToManyField(Teacher,related_name="optional_subject_taught")
+
     def __str__(self):
         return self.name
     
@@ -54,12 +55,12 @@ class StdClass(models.Model):
     all classes in the school
     """
     name = models.CharField(max_length=40)
-    subject = models.ManyToManyField(Subject,related_name="subject")
+    subject = models.ManyToManyField(Subject,related_name="classes")
     optional_subject = models.ManyToManyField(OptionalSubject,
-                                              related_name="optional_subject", null=True, blank=True,
-                                              )
-    class_teacher = models.OneToOneField(Teacher,on_delete=models.SET_NULL,
+                                              related_name="classes", blank=True)
+    class_teacher = models.ForeignKey(Teacher,on_delete=models.SET_NULL,
                                           null=True, blank=True)
+
     def __str__(self):
         return self.name
     
@@ -68,6 +69,10 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20,default="0802")
     address = models.CharField(max_length=50,default="Default address")
+    level = models.CharField(max_length=50)
+    optional_subjects = models.ManyToManyField(OptionalSubject, related_name="students", blank=True)
+    subjects = models.ManyToManyField(Subject, related_name="students", blank=True, limit_choices_to={'subject__lte': 8})
+    dob = models.DateField()
     std_class = models.ForeignKey(StdClass,on_delete=models.CASCADE,default=None)
 
     def __str__(self):
@@ -81,6 +86,7 @@ class SubjectScore(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
     Score = models.IntegerField()
+
     def __str__(self):
         return f"{self.student.user.username} {self.subject.name}"
     
@@ -89,6 +95,7 @@ class OptionalSubjectScore(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
     score = models.IntegerField()
+
     def __str__(self):
         return f"{self.student.user.username} {self.subject.name}"
 
