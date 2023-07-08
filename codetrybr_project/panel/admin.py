adcdab9d6d97dbd33cbc10249259b4bc2e7211b4
olaniyanp11 <olaniyanp11@gmail.com>
2023-07-08 21:25:49 +0100

    updated todo list

--- codetrybr_project/panel/admin.py
+++ codetrybr_project/panel/admin.py
@@ -5,9 +5,21 @@ admin.site.register(Admin)
 admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(Subject)
-admin.site.register(StdClass)
 admin.site.register(SubjectScore)
 admin.site.register(Session)
+from django.contrib import admin
+
+class StdClassAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_students')
+
+    def display_students(self, obj):
+        students = obj.get_students()
+        return ", ".join([str(student) for student in students])
+
+    display_students.short_description = 'Students'
+
+admin.site.register(StdClass, StdClassAdmin)
+
 
 
 # Register your models here.
